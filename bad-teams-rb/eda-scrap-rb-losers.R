2a2136e3b3dff42417c574d65b1a27d411e620d6
Benjamin Arancibia <bcarancibia@gmail.com>
2023-11-16 14:27:09 -0500

    messing with idea around bad rb ideas

--- bad-teams-rb/eda-scrap-rb-losers.R
+++ bad-teams-rb/eda-scrap-rb-losers.R
@@ -17,4 +17,26 @@ rb_snap_counts <- rb_snap_counts %>%
 
 rb_snap_counts
 
-#look at teams in 2022 with losing records
+#look at teams in 2022 with records below .500
+
+unique(rb_snap_counts$team)
+
+#https://www.nfl.com/standings/league/2022/reg
+rb_snap_counts <- rb_snap_counts %>% 
+  filter(team %in% c("CHI", "HOU", "ARI", 
+                     "IND", "DEN", "LA", 
+                     "LV", "ATL", "CAR",
+                     "CLE", "NO", "NYJ",
+                     "TEN", "GB", "NE",
+                     "TB", "WAS"))
+
+
+test_plot <- rb_snap_counts %>% 
+  filter(team %in% "CLE")
+
+ggplot(test_plot, aes(x = week, y = offense_pct, fill = player)) + 
+  geom_bar(stat="identity")
+
+ggplot(rb_snap_counts, aes(x = player, y = offense_pct, fill = team)) + 
+  geom_bar(stat="identity") +
+  facet_wrap(~ week)
